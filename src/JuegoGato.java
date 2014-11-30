290760d30ae00fbfc38c3be43c4d621057f7f0ca
NioxOne <niox_player@hotmail.com>
2014-11-29 19:15:27 -0600

    Repasando Git

--- src/JuegoGato.java
+++ src/JuegoGato.java
@@ -1,7 +1,7 @@
 /*
-Copyright (C) <29/11/2014r>  <Nicolas Lemus Plascencia "Niox">
+    Copyright (C) <29/11/2014r>  <Nicolas Lemus Plascencia "Niox">
 
-  	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -13,7 +13,7 @@ Copyright (C) <29/11/2014r>  <Nicolas Lemus Plascencia "Niox">
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 package bin;
 import java.util.Scanner;
 
